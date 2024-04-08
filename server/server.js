894cdaf1a5474e59ed16de55a7bcec520586859c
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-04-08 18:47:36 -0500

    restart

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require("./utils/auth");
-const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -28,13 +27,11 @@ const startApolloServer = async () => {
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
   }
 
-  app.use(routes);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -45,4 +42,27 @@ const startApolloServer = async () => {
 };
 
 startApolloServer();
+
+
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
 
